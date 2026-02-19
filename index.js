40707d6b589540d29b65072b4fedfec4ecd9547d
blacspot <obwogemcbride73@gmail.com>
2026-02-19 20:10:42 +0300

    addition of multiple crud routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 require('dotenv').config();
+const User = require('./models/user.model');
 
 const app = express()
 app.use(express.json());
@@ -10,9 +11,32 @@ app.get('/', (req, res) => {
     res.send('Hello from node!');
 });
 
-app.post('/adduser', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.get('/getusers', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching users', error: error.message });
+    }
+});
+
+app.post('/adduser', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({ message: 'User added successfully', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Error adding user', error: error.message });
+    }
+});
+
+app.get('/getusers/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const users = await User.findById(id);
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching user by id', error: error.message });
+    }
 });
 
 mongoose.connect(process.env.MONGODB_URI)
