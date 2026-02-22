3a180b8fc2c87f7038ee57bc249040b065fb8abd
blacspot <obwogemcbride73@gmail.com>
2026-02-22 10:03:34 +0300

    addition of update and delete crud operations.

--- index.js
+++ index.js
@@ -39,6 +39,37 @@ app.get('/getusers/:id', async (req, res) => {
     }
 });
 
+app.put('/updateuser/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+
+        if (!user) {
+            return res.status(404).json({message:'User not found'});
+        }
+
+        const updatedUser = await User.findById(id);
+        res.status(200).json({ message: 'User updated successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating user', error: error.message });
+    }
+});
+
+app.delete('/deleteuser/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findByIdAndDelete(id);
+
+        if (!user) {
+            return res.status(404).json({message:'User not found'});
+        }
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+       res.status(500).json({ message: 'Error deleting user', error: error.message });        
+    }
+})
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected!');
